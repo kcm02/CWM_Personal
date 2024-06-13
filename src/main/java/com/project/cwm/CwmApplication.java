a134aaccebf6d7234db803817cdb0ef94bf03964
cm <chanmi125128@gmail.com>
2024-06-13 14:13:27 +0900

    cwm first commit

---
+++ src/main/java/com/project/cwm/CwmApplication.java
@@ -0,0 +1,13 @@
+package com.project.cwm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwmApplication.class, args);
+    }
+
+}
